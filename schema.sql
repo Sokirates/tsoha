018509750175436939753e21345d619ac81b9bc3
Soki <soki.fin02@gmail.com>
2024-06-13 18:01:21 +0300

    new message Table added

--- schema.sql
+++ schema.sql
@@ -1,1 +1,12 @@
-CREATE TABLE areas (id SERIAL PRIMARY KEY, topic TEXT, created_at TIMESTAMP);
+CREATE TABLE areas (
+    id SERIAL PRIMARY KEY, 
+    topic TEXT, 
+    created_at TIMESTAMP
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY,
+    area_id INTEGER REFERENCES areas(id),
+    message TEXT,
+    created_at TIMESTAMP
+);
