fa9c95ef016ee7b73b89de4feee45983a3b3112f
Soki <soki.fin02@gmail.com>
2024-06-13 18:43:58 +0300

    updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE areas (
     id SERIAL PRIMARY KEY, 
-    topic TEXT, 
-    created_at TIMESTAMP
+    topic TEXT NOT NULL, 
+    created_at TIMESTAMP NOT NULL
 );
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     area_id INTEGER REFERENCES areas(id),
-    message TEXT,
-    created_at TIMESTAMP
+    message TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL
 );
