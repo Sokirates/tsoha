9411ed0c20854e703f3256644632f2d85ef23beb
Soki <soki.fin02@gmail.com>
2024-06-13 19:22:29 +0300

    add chatroom functionality

--- app.py
+++ app.py
@@ -29,3 +29,24 @@ def add_discussion_area():
     )
     db.session.commit()
     return redirect("/")
+
+@app.route("/chatroom")
+def chatroom():
+    result = db.session.execute(text("SELECT message, created_at FROM messages"))
+    messages = result.fetchall()
+    return render_template("chatroom.html", messages=messages)
+
+@app.route("/send_message", methods=["POST"])
+def send_message():
+    message = request.form["message"]
+    sql = text("INSERT INTO messages (message, created_at) VALUES (:message, :created_at)")
+    db.session.execute(
+        sql,
+        {"message":message, "created_at":datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
+    )
+    db.session.commit()
+    return redirect("/")
+
+@app.route("/new_message")
+def new_message():
+    return render_template("new_message.html")
