5dda2c681f344dc87a86d55233afcf2f472600ab
Soki <soki.fin02@gmail.com>
2024-06-15 12:18:04 +0300

    able to see messeges

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import datetime
 from flask import Flask
-from flask import redirect, render_template, request
+from flask import redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
@@ -11,9 +11,9 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
-    result = db.session.execute(text("SELECT topic, created_at FROM areas"))
+    result = db.session.execute(text("SELECT id, topic, created_at FROM areas"))
     areas = result.fetchall()
-    return render_template("index.html", count=len(areas), areas=areas) 
+    return render_template("index.html", count=len(areas), areas=areas)
 
 @app.route("/new_area")
 def new():
@@ -30,23 +30,25 @@ def add_discussion_area():
     db.session.commit()
     return redirect("/")
 
-@app.route("/chatroom")
-def chatroom():
-    result = db.session.execute(text("SELECT message, created_at FROM messages"))
+@app.route("/chatroom/<int:id>")
+def chatroom(id):
+    sql = text("SELECT message, created_at FROM messages WHERE area_id = :id")
+    result = db.session.execute(sql, {"id": id})
     messages = result.fetchall()
-    return render_template("chatroom.html", messages=messages)
+    return render_template("chatroom.html", messages=messages, area_id=id)
 
 @app.route("/send_message", methods=["POST"])
 def send_message():
     message = request.form["message"]
-    sql = text("INSERT INTO messages (message, created_at) VALUES (:message, :created_at)")
+    area_id = request.form["area_id"]
+    sql = text("INSERT INTO messages (area_id, message, created_at) VALUES (:area_id, :message, :created_at)")
     db.session.execute(
         sql,
-        {"message":message, "created_at":datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
+        {"area_id": area_id, "message": message, "created_at": datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")}
     )
     db.session.commit()
-    return redirect("/")
+    return redirect(url_for('chatroom', id=area_id))
 
-@app.route("/new_message")
-def new_message():
-    return render_template("new_message.html")
+@app.route("/new_message/<int:area_id>")
+def new_message(area_id):
+    return render_template("new_message.html", area_id=area_id)
