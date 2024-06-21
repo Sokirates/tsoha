87e3ada04618db8eab9b9dcfe0d4bb22a229398c
Soki <soki.fin02@gmail.com>
2024-06-21 17:13:22 +0300

    added creator_id and sender_id references to users table

--- schema.sql
+++ schema.sql
@@ -1,14 +1,16 @@
 CREATE TABLE areas (
     id SERIAL PRIMARY KEY, 
     topic TEXT NOT NULL, 
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    creator_id INTEGER REFERENCES users
 );
 
 CREATE TABLE messages (
     id SERIAL PRIMARY KEY,
     area_id INTEGER REFERENCES areas,
     message TEXT NOT NULL,
-    created_at TIMESTAMP NOT NULL
+    created_at TIMESTAMP NOT NULL,
+    sender_id INTEGER REFERENCES users
 );
 
 CREATE TABLE users (
