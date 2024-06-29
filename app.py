ea5fc665d3cf13b1e42e1f6dd20de1cd7a336e1d
Soki <soki.fin02@gmail.com>
2024-06-29 23:05:27 +0300

    setup to use environment variables

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.secret_key = os.urandom(24)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql+psycopg2://"
+app.secret_key = os.getenv('SECRET_KEY')
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv('DATABASE_URL')
 
 db = SQLAlchemy(app)
 
